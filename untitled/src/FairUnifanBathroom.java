54b804ce79c7d1fe241734d8b156179e0b165105
Samuel Olatunbosun <ayomagic1115@gmail.com>
2025-02-11 03:27:32 -0600

    finished

--- untitled/src/FairUnifanBathroom.java
+++ untitled/src/FairUnifanBathroom.java
@@ -1,22 +1,76 @@
-// EID 1
-// EID 2
+// SAO993
+// DAA3652
 
-public class FairUnifanBathroom {   
-	
+public class FairUnifanBathroom {
+	int max;
+	int curr;
+	int OU_UT;
+	int ticketnumber;
+	int nextticket;
+	public FairUnifanBathroom() {
+		/* Initialization:
+		MAX = 7
+		CURR = 0
+		OU_UT = -1 for OU, 1 for UT and 0 for none */
+		max = 7;
+		curr = 0;
+		OU_UT = 0;
+		ticketnumber = 0;
+		nextticket = 0;
+	}
+
   public synchronized void enterBathroomUT() {
-    // Called when a UT fan wants to enter bathroom	
+    // Called when a UT fan wants to enter bathroom
+
+	  int curr_ticket = get_t();
+	  while (OU_UT == -1 && curr > 0 || OU_UT == 1 && curr >= max || curr_ticket != nextticket) {
+		  try {wait();}
+		  catch (Exception e) {e.printStackTrace();}
+	  }
+	  curr++;
+	  OU_UT = 1;
+	  nextticket++;
   }
 	
 	public synchronized void enterBathroomOU() {
     // Called when a OU fan wants to enter bathroom
+		int curr_ticket = get_t();
+
+		while (OU_UT == 1 && curr > 0 || OU_UT == -1 && curr >= max || curr_ticket != nextticket) {
+			try {wait();}
+			catch (Exception e) {e.printStackTrace();}
+		}
+		curr++;
+		OU_UT = -1;
+		nextticket++;
 	}
 	
 	public synchronized void leaveBathroomUT() {
     // Called when a UT fan wants to leave bathroom
+		if (OU_UT == 1) {
+			curr--;
+			if (curr == 0) {
+				OU_UT = 0;
+			}
+			notifyAll();
+		}
 	}
 
 	public synchronized void leaveBathroomOU() {
     // Called when a OU fan wants to leave bathroom
+		if (OU_UT == -1) {
+			curr--;
+			if (curr == 0) {
+				OU_UT = 0;
+			}
+			notifyAll();
+		}
+	}
+
+	public synchronized int get_t() {
+		int val = this.ticketnumber;
+		this.ticketnumber++;
+		return val;
 	}
 }
 	
--- untitled/src/PriorityQueue.java
+++ untitled/src/PriorityQueue.java
@@ -1,29 +1,152 @@
-// EID 1
-// EID 2
+// SAO993
+// DAA3652
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class PriorityQueue {
+	public class Node {
+		String name;
+		int priority;
+		Node next;
+		final ReentrantLock lock = new ReentrantLock();
+
+		public Node() {
+			this.name = "";
+			this.priority = Integer.MAX_VALUE;
+			this.next = null;
+		}
+		public Node(String name, int priority) {
+			this.name = name;
+			this.priority = priority;
+			this.next = null;
+		}
+	}
+	private final Node head;
+	int capacity;
+	int size;
+	private ReentrantLock queueLock = new ReentrantLock();
+	private final Condition notFull = queueLock.newCondition();
+	private final Condition notEmpty = queueLock.newCondition();
 
 	public PriorityQueue(int maxSize) {
         // Creates a Priority queue with maximum allowed size as capacity
+		head = new Node();
+		this.capacity = maxSize;
+		this.size = 0;
 	}
-
 	public int add(String name, int priority) {
         // Adds the name with its priority to this queue.
         // Returns the current position in the list where the name was inserted;
         // otherwise, returns -1 if the name is already present in the list.
         // This method blocks when the list is full.
-		return 0;
-	}
+		queueLock.lock();
+		try {
+			if (search(name) != -1)
+				return -1;
 
+			while (size >= capacity)
+				notFull.await();
+
+			int pos = 0;
+			head.lock.lock();
+			Node curr = head;
+			Node next = head.next;
+
+			while (next != null && next.priority > priority) {
+				next.lock.lock();
+				curr.lock.unlock();
+				curr = next;
+				next = next.next;
+				pos++;
+			}
+
+			Node newNode = new Node(name, priority);
+			newNode.next = next;
+			curr.next = newNode;
+
+			curr.lock.unlock();  // Unlock the last acquired lock
+			this.size++;
+
+			notEmpty.signal();
+			return pos;
+		} catch (InterruptedException e) {
+			return -1;
+		} finally {
+			queueLock.unlock();
+		}
+
+	}
+	public void printList() {
+		head.lock.lock();
+		Node curr = head.next;
+		while (curr != null) {
+			System.out.print(curr.name + " " + curr.priority + " -> ");
+			curr = curr.next;
+		}
+		head.lock.unlock();
+		System.out.println("null");
+	}
 	public int search(String name) {
         // Returns the position of the name in the list;
         // otherwise, returns -1 if the name is not found.
-		return 0;
-	}
+		head.lock.lock();
+		Node curr = head;
+		Node next = head.next;
+		int pos = 0;
 
+		while (next != null && !next.name.equals(name)) {
+			next.lock.lock();
+			curr.lock.unlock();
+			curr = next;
+			next = next.next;
+			pos++;
+		}
+
+		curr.lock.unlock();
+
+		if (next == null) {
+			return -1;
+		} else {
+			return pos;
+		}
+	}
 	public String getFirst() {
         // Retrieves and removes the name with the highest priority in the list,
         // or blocks the thread if the list is empty.
-		return null;
+		queueLock.lock();
+		try {
+			while (size == 0) {
+				notEmpty.await();
+			}
+
+			head.lock.lock();
+			Node first = head.next;
+
+			if (first == null) {
+				head.lock.unlock();
+				return null;
+			}
+
+			Node next = first.next;
+			first.lock.lock();
+			next.lock.lock();
+
+			head.next = next;
+			first.next = null;
+
+			next.lock.unlock();
+			head.lock.unlock();
+			first.lock.unlock();
+
+			this.size --;
+			notFull.signal();
+
+			return first.name;
+		} catch (InterruptedException e) {
+			return null;
+		} finally {
+			queueLock.unlock();
+		}
 	}
 }
